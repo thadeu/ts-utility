cc743ea28a27a8f620ffb5f9201ac948da2f9907
Thadeu Esteves Jr <tadeuu@gmail.com>
2023-12-01 15:39:41 -0300

    v1.0.2

--- dist/Util.d.ts
+++ dist/Util.d.ts
@@ -1,5 +1,4 @@
-import { isFunction } from 'lodash';
-export { isFunction };
+export declare const isFunction: (fn: any) => boolean;
 export declare const isAsyncFunction: (fn: any) => boolean;
 export declare const execute: (o: any, params?: any) => any;
 export declare const delay: (ms: any) => Promise<unknown>;
